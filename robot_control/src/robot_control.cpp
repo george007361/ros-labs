d54123a2de57f32b7166d73a9ab9941af21c4773
George007361 <gera.illarionov@mail.ru>
2023-04-02 14:55:43 +0300

    Task 1

--- robot_control/src/robot_control.cpp
+++ robot_control/src/robot_control.cpp
@@ -5,130 +5,58 @@
 bool obstacle = false;
 ros::Publisher pub;
 
-/**
- * Функция, которая будет вызвана
- * при получении данных от лазерного дальномера
- * параметр функции msg - ссылка на полученное сообщение
- */
-void laserCallback(const sensor_msgs::LaserScan& msg)
+void laserCallback(const sensor_msgs::LaserScan &msg)
 {
-  ROS_DEBUG_STREAM("Laser msg: "<<msg.scan_time);
+  ROS_DEBUG_STREAM("Laser msg: " << msg.scan_time);
 
-  const double kMinRange = 0.3;
-  //проверим нет ли вблизи робота препятствия
-  for (size_t i = 0; i<msg.ranges.size(); i++)
+  const double kMinRange = 0.5;
+  obstacle = false;
+
+  for (size_t i = 0; i < msg.ranges.size(); i++)
   {
-      if (msg.ranges[i] < kMinRange)
-	  {
-		  obstacle = true;
-		  ROS_WARN_STREAM("OBSTACLE!!!");
-		  break;
-	  }
+    if (msg.ranges[i] < kMinRange)
+    {
+      obstacle = true;
+      break;
+    }
   }
 }
 
-
-/**
- * Функция, которая будет вызвана при
- * получении сообщения с текущем положением робота
- * параметр функции msg - ссылка на полученное сообщение
- */
-
-void poseCallback(const nav_msgs::Odometry& msg)
+void poseCallback(const nav_msgs::Odometry &msg)
 {
-  ROS_DEBUG_STREAM("Pose msg: x = " << msg.pose.pose.position.x<<
-          " y = " << msg.pose.pose.position.y <<
-          " theta = " << 2*atan2(msg.pose.pose.orientation.z,
-                  msg.pose.pose.orientation.w) );
+  ROS_DEBUG_STREAM("Pose msg: x = " << msg.pose.pose.position.x << " y = " << msg.pose.pose.position.y << " theta = " << 2 * atan2(msg.pose.pose.orientation.z, msg.pose.pose.orientation.w));
 }
-/*
- * функция обработчик таймера
- * параметр функции - структура, описывающая событие таймера, здесь не используется
- */
-void timerCallback(const ros::TimerEvent&)
-{  
-	static int counter = 0;
-	counter++;
-	ROS_DEBUG_STREAM("on timer "<<counter);
-	//сообщение с помощью которого задается
-	//управление угловой и линейной скоростью
-	geometry_msgs::Twist cmd;
-	//при создании структура сообщения заполнена нулевыми значениями
-	//если вблизи нет препятствия то задаем команды
-    if (!obstacle)
-	{
-        if (counter % 30 > 15)
-		{
-			ROS_INFO_STREAM("go left");
-			cmd.linear.x = 0.5;
-			cmd.angular.z = 0.5;
-		}
-		else
-		{
-			ROS_INFO_STREAM("go right");
-			cmd.linear.x = 0.5;
-			cmd.angular.z = -0.5;
-		}
-	}
-	//отправляем (публикуем) команду
-	pub.publish(cmd);
+
+void timerCallback(const ros::TimerEvent &)
+{
+  static int counter = 0;
+  counter++;
+  geometry_msgs::Twist cmd;
+  if (!obstacle)
+  {
+    cmd.linear.x = 1;
+  }
+  else
+  {
+    cmd.linear.x = 0;
+    cmd.angular.z = 0.5;
+  }
+
+  pub.publish(cmd);
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * Инициализация системы сообщений ros
-   * Регистрация node с определенным именем (третий аргумент функции)
-   * Эта функция должна быть вызвана в первую очередь
-   */
   ros::init(argc, argv, "control_node");
-
-  /**
-   * NodeHandle  - объект через который осуществляется взаимодействие с ROS:
-   * передача сообщений
-   * регистрация коллбаков (функций обработки сообщений)
-   */
   ros::NodeHandle n;
 
-  /**
-   * subscribe() функция подписки на сообщения определенного типа,
-   * передаваемое по заданному топику
-   * В качестве параметров указываются
-   * - топик - на сообщения которого происходит подписка
-   * - длина очереди сообщений хранящихся до обработки (если очередь заполняется,
-   *  то самые старые сообщения будут автоматически удаляться )
-   *  - функция обработки сообщений
-   *
-   *
-   *  Подписываемся на данные дальномера
-
-   */
   ros::Subscriber laser_sub = n.subscribe("base_scan", 1, laserCallback);
-
-  /*
-   * Подписываемся на данные о положении робота
-   */
   ros::Subscriber pose_sub = n.subscribe("base_pose_ground_truth", 1, poseCallback);
 
+  ros::Timer timer1 = n.createTimer(ros::Duration(0.001), timerCallback);
 
-  /*
-   * Регистрируем функцию обработчик таймера 10Hz
-   */
-  ros::Timer timer1 = n.createTimer(ros::Duration(0.1), timerCallback);
-
-  /*
-   * Сообщаем, что мы будем публиковать сообщения типа Twist по топику cmd_vel
-   * второй параметр - длина очереди
-   */
   pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-   /**
-   * ros::spin() функция внутри которой происходит вся работа по приему сообщений
-   * и вызову соответствующих обработчиков . Вся обработка происходит из основного потока
-   * (того, который вызвал ros::spin())
-   * Функция будет завершена, когда подьзователь прервет выполнение процесса с Ctrl-C
-   *
-   */
   ros::spin();
 
   return 0;
